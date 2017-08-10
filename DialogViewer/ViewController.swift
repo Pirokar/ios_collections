2d8a605b17fd4962748d800d7c1fb13b5af61faf
Vladimir Rybalka <vladimir@Vladimirs-Mac.local>
2017-08-10 12:30:41 +0300

    Initial Commit

---
+++ DialogViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DialogViewer
+//
+//  Created by Vladimir Rybalka on 10/08/2017.
+//  Copyright © 2017 Vladimir Rybalka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
